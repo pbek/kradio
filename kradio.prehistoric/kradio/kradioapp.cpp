f1d3f96cc2e86021cd59c62ed788bd506ed1464e
Klas Kalass <kalass@sourceforge.net>
2002-02-09 18:20:55 +0000

    added kradioapp this is the main application class, that holds the central objects.
    git-svn-id: https://kradio.svn.sourceforge.net/svnroot/kradio/trunk@19 1c7eebff-f921-0410-8e14-d196b13b6ee1

---
+++ kradio.prehistoric/kradio/kradioapp.cpp
@@ -0,0 +1,121 @@
+/**************************************************************************
+                          kradioapp.cpp  -  description
+                             -------------------
+    begin                : Sa Feb  9 CET 2002
+    copyright            : (C) 2002 by Klas Kalass
+    email                : klas.kalass@gmx.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "kradio.h"
+#include "kradioapp.h"
+#include "docking.h"
+#include <kiconloader.h>
+#include <kconfig.h>
+
+KRadioApp::KRadioApp()
+  :kradio(0),
+   tray(0)
+{
+  config = KGlobal::config(); 
+  readOptions();
+
+  // the actual radio
+  radio = new V4LRadio(this, "", RadioDev, MixerDev);
+  radio->readCfg (QString(getenv("HOME")) + "/.kradiorc");
+
+  // the main dialog
+  kradio = new KRadio(radio);
+  
+  // Tray
+  tray = new RadioDocking(kradio, radio);
+  if (tray)
+    tray->setPixmap(BarIcon("kradio"));
+
+  // restore window size and position
+  if (kradio)
+    kradio->readXOptions(config);
+
+  // restore Configuration
+  restoreConfig();
+
+  if (kradio){
+    connect(kradio, SIGNAL(showAbout()),
+	    &AboutApplication, SLOT(show()));
+    kradio->show();
+  }
+  if (tray){
+    connect(tray, SIGNAL(showAbout()),
+	    &AboutApplication, SLOT(show()));
+    tray->show();
+  }
+
+}
+
+KRadioApp::~KRadioApp()
+{
+  saveOptions();
+  saveConfig();
+
+  if (kradio){
+    kradio->saveOptions(config);
+    delete kradio;
+  }
+  if (radio)
+    delete radio;
+  radio = 0;
+}
+
+void KRadioApp::readOptions()
+{
+  config->setGroup("Recent");
+  
+  RadioDev = config->readEntry ("RadioDev", "/dev/radio");
+  MixerDev = config->readEntry ("MixerDev", "/dev/mixer");	
+  
+}
+
+void KRadioApp::saveOptions()
+{
+  config->writeEntry("MixerDev", MixerDev);
+  config->writeEntry("RadioDev", RadioDev);
+}
+
+void KRadioApp::restoreConfig()
+{
+  config->setGroup("Recent");
+  float freq = config->readDoubleNumEntry("Frequency", 88);
+  radio->setFrequency(freq);
+  unsigned int vol = config->readNumEntry("Volume", 65535);
+  radio->setVolume(vol);
+  
+  if (config->readBoolEntry ("PowerOn"))
+    radio->PowerOn();
+
+  // update kradio
+  if (kradio)
+    kradio->slotPowerOn (radio->isPowerOn());
+}
+
+void KRadioApp::saveConfig()
+{
+  config->setGroup("Recent");
+  if (radio)
+    config->writeEntry("Frequency", radio->getFrequency());
+  config->writeEntry("Volume", radio->getVolume());
+  
+  config->writeEntry("PowerOn", radio->isPowerOn());
+}
+
---
+++ kradio.prehistoric/kradio/kradioapp.h
@@ -0,0 +1,60 @@
+/***************************************************************************
+                          kradioapp.h  -  description
+                             -------------------
+    begin                : Sa Feb  9 2002
+    copyright            : (C) 2002 by Klas Kalass
+    email                : klas.kalass@gmx.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+
+#ifndef KRADIOAPP_H
+#define KRADIOAPP_H
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <kapplication.h>
+#include <qstring.h>
+#include <kaboutapplication.h>
+
+#include "v4lradio.h"
+
+class RadioDocking;
+class KRadio;
+
+class KRadioApp : public KApplication 
+{
+ public:
+  KRadioApp();
+  virtual ~KRadioApp();
+  
+ private:
+  void restoreConfig();
+  void readOptions();
+
+  void saveConfig();
+  void saveOptions();
+
+  KRadio *kradio;
+  KAboutApplication AboutApplication;
+  
+  KConfig *config;
+  RadioDocking *tray;
+  V4LRadio *radio;
+  
+  QString MixerDev, RadioDev;
+
+};
+
+#endif
