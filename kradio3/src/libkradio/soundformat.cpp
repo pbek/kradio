a2818f0cb2a2ec5c54a079963c8b38fa675192de
Ernst Martin Witte <emw@sourceforge.net>
2005-05-18 21:10:18 +0000

    more cleanups
    git-svn-id: https://kradio.svn.sourceforge.net/svnroot/kradio/trunk@275 1c7eebff-f921-0410-8e14-d196b13b6ee1

---
+++ kradio3/src/libkradio/soundformat.cpp
@@ -0,0 +1,268 @@
+/***************************************************************************
+                     soundformat.cpp  -  description
+                             -------------------
+    begin                : Sun Aug 1 2004
+    copyright            : (C) 2004 by Martin Witte
+    email                : witte@kawo1.rwth-aachen.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "soundformat.h"
+
+int SoundFormat::sampleSize() const
+{
+    if (m_SampleBits <= 8)  return 1;
+    if (m_SampleBits <= 16) return 2;
+    if (m_SampleBits <= 32) return 4;
+
+    // unknown
+    return -1;
+}
+
+
+int SoundFormat::frameSize() const
+{
+    return sampleSize() * m_Channels;
+}
+
+
+int SoundFormat::minValue() const
+{
+    if (!m_IsSigned) return 0;
+    return -(1 << (m_SampleBits - 1));
+}
+
+
+int SoundFormat::maxValue() const
+{
+    return (1 << (m_SampleBits - m_IsSigned)) - 1;
+}
+
+
+void  SoundFormat::restoreConfig(const QString &prefix, KConfig *c)
+{
+    m_SampleBits      = c->readNumEntry (prefix + "bits", 16);
+    m_IsSigned        = c->readBoolEntry(prefix + "sign", true);
+    m_Channels        = c->readNumEntry (prefix + "channels", 2);
+    m_SampleRate      = c->readNumEntry (prefix + "samplerate", 44100);
+    bool littleEndian = c->readBoolEntry(prefix + "littleEndian", true);
+    m_Endianess = littleEndian ? LITTLE_ENDIAN : BIG_ENDIAN;
+}
+
+
+void  SoundFormat::saveConfig(const QString &prefix, KConfig *c) const
+{
+    c->writeEntry(prefix + "bits",         m_SampleBits);
+    c->writeEntry(prefix + "sign",         m_IsSigned);
+    c->writeEntry(prefix + "channels",     m_Channels);
+    c->writeEntry(prefix + "samplerate",   m_SampleRate);
+    c->writeEntry(prefix + "littleEndian", m_Endianess == LITTLE_ENDIAN);
+}
+
+
+int SoundFormat::convertSampleToInt(const char *sample, bool do_scale) const
+{
+    int size     = sampleSize();
+
+    unsigned val = 0;
+    if (m_Endianess == LITTLE_ENDIAN) {
+        sample = sample + size - 1;
+        for (int i = size - 1; i >= 0; --i, --sample) {
+            val = (val << 8) | (unsigned char)*sample;
+        }
+    } else {
+        for (int i = 0; i < size; ++i, ++sample) {
+            val = (val << 8) | (unsigned char)*sample;
+        }
+    }
+
+    int scale    = (sizeof(unsigned) << 3) - m_SampleBits;
+    int signmask = do_scale ? (!m_IsSigned << ((sizeof(unsigned) << 3) - 1)) :
+                              (-m_IsSigned << ((size << 3)             - 1)) ;
+    if (do_scale) {
+        // map to int number space
+        return (val << scale) ^ signmask;
+    } else {
+        // do only sign extension
+        if (val & signmask)
+            val |= signmask;
+        return val;
+    }
+}
+
+
+void SoundFormat::convertIntToSample(int src, char *dst, bool is_scaled) const
+{
+    int size     = sampleSize();
+    int scale    = (sizeof(unsigned) * 8) - m_SampleBits;
+    int signmask = (!m_IsSigned << (sizeof(unsigned) * 8 - 1));
+
+    unsigned val = is_scaled ? (src ^ signmask) >> scale : src;
+    if (m_Endianess == LITTLE_ENDIAN) {
+        for (int i = 0; i < size; ++i, ++dst) {
+            (unsigned char &)*dst = val & 0xFF;
+             val >>= 8;
+        }
+    } else {
+        dst += size - 1;
+        for (int i = size - 1; i >= 0; --i, --dst) {
+            (unsigned char &)*dst = val & 0xFF;
+             val >>= 8;
+        }
+    }
+}
+
+
+void SoundFormat::convertSamplesToInts(const char *src, int *dst, unsigned n, bool do_scale) const
+{
+    int size     = sampleSize();
+    int scale    = (sizeof(unsigned) * 8) - m_SampleBits;
+    int signmask = do_scale ? (!m_IsSigned << ((sizeof(unsigned) << 3) - 1)) :
+                              (-m_IsSigned << ((size << 3)             - 1)) ;
+    if (m_Endianess == LITTLE_ENDIAN) {
+        src += (size * n) - 1;
+        int *end = dst;
+        for (dst += n - 1; dst >= end; --dst) {
+            unsigned val = 0;
+            for (int i = size - 1; i >= 0; --i, --src) {
+                val = (val << 8) | (unsigned char)*src;
+            }
+            if (do_scale) {
+                *dst = (val << scale) ^ signmask;
+            } else if (val & signmask) {
+                *dst = val | signmask;
+            }
+        }
+    } else {
+        for (int *end = dst + n; dst < end; ++dst) {
+            unsigned val = 0;
+            for (int i = 0; i < size; ++i, ++src) {
+                val = (val << 8) | (unsigned char)*src;
+            }
+            if (do_scale) {
+                *dst = (val << scale) ^ signmask;
+            } else if (val & signmask) {
+                *dst = val | signmask;
+            }
+        }
+    }
+}
+
+
+void SoundFormat::convertIntsToSamples(const int *src, char *dst, unsigned n, bool is_scaled) const
+{
+    int size     = sampleSize();
+    int scale    = (sizeof(unsigned) * 8) - m_SampleBits;
+    int signmask = (!m_IsSigned << (sizeof(unsigned) * 8 - 1));
+
+    if (m_Endianess == LITTLE_ENDIAN) {
+        for (const int *end = src+n; src < end; ++src) {
+            unsigned val = is_scaled ? ((unsigned)(*src ^ signmask)) >> scale : *src;
+            for (int i = 0; i < size; ++i, ++dst) {
+                (unsigned char &)*dst = val & 0xFF;
+                val >>= 8;
+            }
+        }
+    } else {
+        dst += (size * n) - 1;
+        const int *end = src;
+        for (src += n - 1; src >= end; --src) {
+            unsigned val = is_scaled ? ((unsigned)(*src ^ signmask)) >> scale : *src;
+            for (int i = size - 1; i >= 0; --i, --dst) {
+                (unsigned char &)*dst = val & 0xFF;
+                val >>= 8;
+            }
+        }
+    }
+}
+
+
+void SoundFormat::convertSamplesToFloat(const char *_src, float **_dst, unsigned n) const
+{
+    int sample_size = sampleSize();
+    int frame_size  = frameSize();
+    int scale       = (sizeof(short) << 3) - m_SampleBits;
+    int signmask    = !m_IsSigned << ((sizeof(short) << 3) - 1);
+    int skip        = frame_size - sample_size;
+
+    if (m_Endianess == LITTLE_ENDIAN) {
+        const char *src_ch0_end = _src + frame_size * (n - 1) + sample_size - 1;
+        for (unsigned ch = 0; ch < m_Channels; ++ch) {
+            const char *src = src_ch0_end + sample_size * ch;
+            float      *dst = _dst[ch];
+            float      *end = dst;
+            for (dst += n - 1; dst >= end; --dst) {
+                unsigned val = 0;
+                for (int i = sample_size - 1; i >= 0; --i, --src) {
+                    val = (val << 8) | (unsigned char)*src;
+                }
+                *dst = (float)(signed short)((val << scale) ^ signmask) / 32768.0f;
+                src -= skip;
+            }
+        }
+    } else {
+        for (unsigned ch = 0; ch < m_Channels; ++ch) {
+            const char *src = _src + sample_size * ch;
+            float      *dst = _dst[ch];
+            for (float *end = dst + n; dst < end; ++dst) {
+                unsigned val = 0;
+                for (int i = 0; i < sample_size; ++i, ++src) {
+                    val = (val << 8) | (unsigned char)*src;
+                }
+                *dst = (float)(signed short)((val << scale) ^ signmask) / 32768.0f;
+                src += skip;
+            }
+        }
+    }
+}
+
+
+
+void SoundFormat::convertFloatsToSamples(const float **_src, char *_dst, unsigned n) const
+{
+    int sample_size = sampleSize();
+    int frame_size  = frameSize();
+    int scale       = (sizeof(short) << 3) - m_SampleBits;
+    int signmask    = (!m_IsSigned << (sizeof(short) << 3) - 1);
+    int skip        = frame_size - sample_size;
+
+    if (m_Endianess == LITTLE_ENDIAN) {
+        for (unsigned ch = 0; ch < m_Channels; ++ch) {
+            const float *src = _src[ch];
+            char        *dst = _dst + ch * sample_size;
+            for (const float *end = src+n; src < end; ++src) {
+                unsigned val = ((  ((unsigned)(*src * 32768.0f)) ^ signmask)) >> scale;
+                for (int i = 0; i < sample_size; ++i, ++dst) {
+                    (unsigned char &)*dst = val & 0xFF;
+                    val >>= 8;
+                }
+                dst += skip;
+            }
+        }
+    } else {
+        char  *dst_ch0_end = _dst + frame_size * (n - 1) + sample_size - 1;
+        for (unsigned ch = 0; ch < m_Channels; ++ch) {
+            char        *dst = dst_ch0_end + sample_size * ch;
+            const float *src = _src[ch];
+            const float *end = src;
+            for (src += n - 1; src >= end; --src) {
+                unsigned val = (( ((unsigned)(*src * 32768.0f)) ^ signmask)) >> scale;
+                for (int i = sample_size - 1; i >= 0; --i, --dst) {
+                    (unsigned char &)*dst = val & 0xFF;
+                    val >>= 8;
+                }
+                dst -= skip;
+            }
+        }
+    }
+}
+
+
---
+++ kradio3/src/libkradio/soundformat.h
@@ -0,0 +1,75 @@
+/***************************************************************************
+                      soundformat.h  -  description
+                             -------------------
+    begin                : Sun Aug 1 2004
+    copyright            : (C) 2004 by Martin Witte
+    email                : witte@kawo1.rwth-aachen.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef KRADIO_SOUNDFORMAT_H
+#define KRADIO_SOUNDFORMAT_H
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <endian.h>
+#include <qstring.h>
+#include <kconfig.h>
+
+struct SoundFormat {
+    unsigned     m_SampleRate;
+    unsigned     m_Channels;
+    unsigned     m_SampleBits;
+    bool         m_IsSigned;
+    unsigned     m_Endianess;
+    QString      m_Encoding;     // "raw", "mp3", ...  (no "wav", because it's only header + raw data)
+
+    SoundFormat(unsigned sample_rate, unsigned channels, unsigned sample_bits, bool is_signed, unsigned endianess, const QString &enc)
+        : m_SampleRate(sample_rate), m_Channels(channels), m_SampleBits(sample_bits), m_IsSigned(is_signed), m_Endianess(endianess), m_Encoding(enc) {}
+    SoundFormat(unsigned sample_rate, unsigned channels, unsigned sample_bits, bool is_signed, unsigned endianess)
+        : m_SampleRate(sample_rate), m_Channels(channels), m_SampleBits(sample_bits), m_IsSigned(is_signed), m_Endianess(endianess), m_Encoding("raw") {}
+    SoundFormat(unsigned sample_rate, unsigned channels, unsigned sample_bits, bool is_signed)
+        : m_SampleRate(sample_rate), m_Channels(channels), m_SampleBits(sample_bits), m_IsSigned(is_signed), m_Endianess(BYTE_ORDER), m_Encoding("raw") {}
+    SoundFormat(bool stereo)
+        : m_SampleRate(44100), m_Channels(stereo ? 2 : 1), m_SampleBits(16), m_IsSigned(true), m_Endianess(BYTE_ORDER), m_Encoding("raw") {}
+    SoundFormat()
+        : m_SampleRate(44100), m_Channels(2), m_SampleBits(16), m_IsSigned(true), m_Endianess(BYTE_ORDER), m_Encoding("raw") {}
+
+    bool operator == (const SoundFormat &o) const { return m_SampleRate == o.m_SampleRate &&
+                                                           m_Channels   == o.m_Channels &&
+                                                           m_SampleBits == o.m_SampleBits &&
+                                                           m_IsSigned   == o.m_IsSigned &&
+                                                           m_Endianess  == o.m_Endianess &&
+                                                           m_Encoding   == o.m_Encoding
+                                                    ;
+                                                   }
+    bool operator != (const SoundFormat &o) const  { return !operator == (o); }
+
+    int      sampleSize() const;      // size of a single sample
+    int      frameSize() const;       // sampleSize * channels
+    int      minValue() const;
+    int      maxValue() const;
+
+    void     restoreConfig(const QString &prefix, KConfig *c);
+    void     saveConfig(const QString &prefix, KConfig *c) const;
+
+    int      convertSampleToInt(const char *sample, bool do_scale) const;
+    void     convertIntToSample(int src, char *dst, bool is_scaled) const;
+    void     convertSamplesToInts(const char *src, int  *dst, unsigned n, bool do_scale) const;
+    void     convertIntsToSamples(const int  *src, char *dst, unsigned n, bool is_scaled) const;
+    void     convertSamplesToFloat (const char   *src, float **dst, unsigned n) const;
+    void     convertFloatsToSamples(const float **src,   char *dst, unsigned n) const;
+};
+
+
+#endif
