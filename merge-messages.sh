d030997a5c662a30382e76d8b0f12ca842b46bed
Ernst Martin Witte <emw@sourceforge.net>
2009-04-18 11:04:16 +0000

    added missing .po files for partial translated languages
    git-svn-id: https://kradio.svn.sourceforge.net/svnroot/kradio/trunk@831 1c7eebff-f921-0410-8e14-d196b13b6ee1

--- merge-messages.sh
+++ merge-messages.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+MIN_LANGS="cs de es pl pt ru uk"
+
 echo "Merging translations"
-catalogs=`find . -name '*.po'`
+#catalogs=`find . -name '*.po'`
+catalogs=`find -iname "de.po" -o -name "*.po" | while read line ; do echo $line ; for LANG in $MIN_LANGS ; do echo $line | sed "s:de\.po:$LANG.po:" ; done ; done | sort | uniq`
+
 for cat in $catalogs; do
+    test -e $cat || touch $cat
     echo $cat
     catdir=$(dirname "$cat")
     pots=`find "$catdir" -maxdepth 1 -a -name "*.pot"`
