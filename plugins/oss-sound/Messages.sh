bbbff41fde491ca2817480d9640c7017ab671115
Ernst Martin Witte <emw@sourceforge.net>
2009-04-18 10:13:22 +0000

    merged translation files with recent message strings. update of german(de) translation
    git-svn-id: https://kradio.svn.sourceforge.net/svnroot/kradio/trunk@830 1c7eebff-f921-0410-8e14-d196b13b6ee1

--- plugins/oss-sound/Messages.sh
+++ plugins/oss-sound/Messages.sh
@@ -2,7 +2,7 @@
 
 $EXTRACTRC    *.rc *.ui *.kcfg >> rc.cpp
 #$PREPARETIPS   > tips.cpp
-$XGETTEXT     *.cpp *.h -o $podir/kradio4_plugin_alsa_sound.pot
+$XGETTEXT     *.cpp *.h -o $podir/kradio4_plugin_oss_sound.pot
 
 
 rm -f rc.cpp tips.cpp
