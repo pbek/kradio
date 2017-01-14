pbek/kradio

commit 34d48e020c5a759458eb73c1f6d6ec0fcf60c117

docking: simplify tooltip creation
when composing the string for tray tooltip, concatenate strings only
when needed (i.e. there is a non-empty RDS text)

plugins/gui-docking-menu/docking.cpp
@@ -501,7 +501,11 @@ bool RadioDocking::noticeStationChanged (const RadioStation &rs, int /*idx*/)
     const QString s = rs.isValid() ? rs.longName() : i18n("invalid station");
 
     const QString &rt = queryRDSRadioText();
-    setToolTip(s + (rt.length() ? "\n" + rt : ""));
+    QString tooltip = rt;
+    if (!rt.isEmpty()) {
+        tooltip += QString::fromLatin1("\n") + rt;
+    }
+    setToolTip(tooltip);
 
     bool r = false;
     SoundFormat sf;
@@ -955,7 +959,11 @@ void RadioDocking::setWheelAction(SystrayWheelAction action)
 bool RadioDocking::noticeRDSRadioTextChanged(const QString &s)
 {
     const RadioStation &rs = queryCurrentStation();
-    setToolTip(rs.longName() + (s.length() ? "\n" + s : ""));
+    QString tooltip = rs.longName();
+    if (!s.isEmpty()) {
+        tooltip += QString::fromLatin1("\n") + s;
+    }
+    setToolTip(tooltip);
     return true;
 }
 

commit dbf612b81ecb9b2afd724d22293a094d88305102

docking: fix tooltips on station change
fix a stupid copy&paste mistake I introduced in
commit 34d48e020c5a759458eb73c1f6d6ec0fcf60c117

plugins/gui-docking-menu/docking.cpp
@@ -502,7 +502,7 @@ bool RadioDocking::noticeStationChanged (const RadioStation &rs, int /*idx*/)
     const QString s = rs.isValid() ? rs.longName() : i18n("invalid station");
 
     const QString &rt = queryRDSRadioText();
-    QString tooltip = rt;
+    QString tooltip = s;
     if (!rt.isEmpty()) {
         tooltip += QString::fromLatin1("\n") + rt;
     }