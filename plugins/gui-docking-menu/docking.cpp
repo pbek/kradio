pbek/kradio

commit 8b8826902f6587d8d4745bc2c49c4b9a156f4a10

docking: cache the string of the next alarm
keep a cache of the string representing the next alarm, and rebuild
the whole context menu only if that string changes

this avoids 3 rebuilds of the context menu during statup and shutdown,
and useless queries of the next alarm every time the menu is rebuilt

plugins/gui-docking-menu/docking.cpp
@@ -108,6 +108,8 @@ RadioDocking::RadioDocking(const QString &instanceID, const QString &name)
     // for now let's add the system tray menu
     setContextMenu(NULL);
 
+    m_cachedNextAlarmString = generateAlarmTitle(queryNextAlarm());
+
     buildContextMenu ();
 
 //     m_WorkaroundRecordingMenuUpdate.setInterval(100);
@@ -273,7 +275,7 @@ void RadioDocking::buildContextMenu()
     buildStationList(queryStations());
 
 
-    m_alarmID  = m_menu->addTitle (generateAlarmTitle());
+    m_alarmID  = m_menu->addTitle (m_cachedNextAlarmString);
 
     m_sleepID  = m_menu->addAction(KIcon("kradio_zzz"), "sleep-dummy");
     m_seekfwID = m_menu->addAction(KIcon("media-seek-forward"),  i18n("Search Next Station"));
@@ -421,9 +423,12 @@ void RadioDocking::slotSleepCountdown()
 }
 
 
-bool RadioDocking::noticeNextAlarmChanged(const Alarm */*a*/)
+bool RadioDocking::noticeNextAlarmChanged(const Alarm *a)
 {
-    buildContextMenu();
+    const QString newTitle = generateAlarmTitle(a);
+    if (newTitle != m_cachedNextAlarmString) {
+        buildContextMenu();
+    }
     return true;
 }
 
@@ -479,14 +484,13 @@ QString RadioDocking::generateStationTitle() const
     return i18n("KRadio: %1", s);
 }
 
-QString RadioDocking::generateAlarmTitle() const
+QString RadioDocking::generateAlarmTitle(const Alarm *a) const
 {
     QDateTime d;
-    const Alarm *a = queryNextAlarm();
     if (a) d = a->nextAlarm();
 
     if (d.isValid()) {
-        return i18n("next alarm: %1", a->nextAlarm().toString());
+        return i18n("next alarm: %1", d.toString());
     } else {
         return i18n("<no alarm pending>");
     }

plugins/gui-docking-menu/docking.h
@@ -160,7 +160,7 @@ protected:
     void    buildRecordingMenu();
     void    buildStationList(const StationList &sl, QAction *before = 0);
     QString generateStationTitle() const;
-    QString generateAlarmTitle  () const;
+    QString generateAlarmTitle  (const Alarm *) const;
 
     void    ShowHideWidgetPlugins();
 
@@ -217,6 +217,8 @@ protected:
 
 //     QList<QAction*>                            m_WorkaroundRecordingMenuActionsToBeDeleted;
 //     QTimer                                     m_WorkaroundRecordingMenuUpdate;
+
+    QString                                    m_cachedNextAlarmString;
 };
 
 

commit 91c9b1355e7685ce71efc279b8bd6b316a3ae58a

docking: fix caching of alarm titles
when the title changes, properly cache it; small regression
I introduced in commit 8b8826902f6587d8d4745bc2c49c4b9a156f4a10

plugins/gui-docking-menu/docking.cpp
@@ -427,6 +427,7 @@ bool RadioDocking::noticeNextAlarmChanged(const Alarm *a)
 {
     const QString newTitle = generateAlarmTitle(a);
     if (newTitle != m_cachedNextAlarmString) {
+        m_cachedNextAlarmString = newTitle;
         buildContextMenu();
     }
     return true;